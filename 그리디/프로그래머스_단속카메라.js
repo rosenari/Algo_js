18e924e2a6cdffa649f8589f8df5141a363405e2
임장순 <rosenari88@gmail.com>
2021-04-26 19:50:35 +0900

    프로그래머스 단속카메라

---
+++ 그리디/프로그래머스_단속카메라.js
@@ -0,0 +1,13 @@
+function solution(routes) {
+    routes.sort((a,b) => a[1]-b[1]);
+    
+    let camera = routes[0][1];
+    let answer = 1;
+    for(let i=1;i<routes.length;i++){
+        if(camera<routes[i][0]){
+            answer++;
+            camera = routes[i][1];
+        }
+    }
+    return answer;
+}
