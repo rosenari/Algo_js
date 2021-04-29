15d5dd67db601b48ab31e5233fa36a52c4bfe7c7
임장순 <rosenari88@gmail.com>
2021-04-29 16:05:30 +0900

    프로그래머스 다리를지나는트럭

--- 스택_큐/프로그래머스_다리를지나는트럭.js
+++ 스택_큐/프로그래머스_다리를지나는트럭.js
@@ -1,29 +1,28 @@
 function solution(bridge_length, weight, truck_weights) {
-    let bridge = [];
     let answer = 0;
-    while(bridge.length > 0 || truck_weights.length > 0){   
+    let bridge = [];
+    while(truck_weights.length > 0 || bridge.length > 0){
+        let sum = 0;
         if(bridge.length > 0){
             for(let i=0;i<bridge.length;i++){
-                bridge[i][1]+=1;
+                let truck = bridge[i];
+                truck[1]+=1;
+                if(truck[1] >= bridge_length){
+                    bridge.shift();
+                    i--;
+                }else{
+                    sum+=truck[0];
+                }
             }
-            if(bridge[0][1]>bridge_length) bridge.shift(); 
         }
         
-        if(truck_weights.length > 0 
-           && getWeight(bridge)+truck_weights[0]<=weight
-           && bridge.length < bridge_length){
-                bridge.push([truck_weights.shift(),1]);
+        if(truck_weights.length > 0){
+            if(sum+truck_weights[0] <= weight && bridge.length < bridge_length){
+                bridge.push([truck_weights.shift(),0]);
+            }
         }
         
         answer++;
-    }
+    }    
     return answer;
-}
-
-function getWeight(bridge){
-    let sum = 0;
-    for(let truck of bridge){
-        sum+=truck[0];
-    }
-    return sum;
 }
