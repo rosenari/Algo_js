f1857bebb11253e81aec21b04d71396c88a1259a
임장순 <rosenari88@gmail.com>
2021-05-02 20:02:02 +0900

    Fix / 프로그래머스 가장먼노드

--- 다익스트라/프로그래머스_가장먼노드.js
+++ 다익스트라/프로그래머스_가장먼노드.js
@@ -1,13 +1,13 @@
 function solution(n, edge) {
+    return Djikstra(n,edge);
+}
+
+function Djikstra(n,edge){
     let adjList = new Array(n+1);
-    
-    for(let i=0;i<adjList.length;i++){
-        adjList[i] = [];
-    }    
-    
-    for(let e of edge){
-        let from = e[0];
-        let to = e[1];
+    for(let i=0;i<adjList.length;i++) adjList[i] = [];
+    for(let i=0;i<edge.length;i++){
+        let from = edge[i][0];
+        let to = edge[i][1];
         adjList[from].push(to);
         adjList[to].push(from);
     }
@@ -15,36 +15,28 @@ function solution(n, edge) {
     let v = new Array(n+1).fill(false);
     let d = new Array(n+1).fill(Infinity);
     d[1] = 0;
+    let queue = [];
+    queue.push([1,d[1]]);
     
-    Dijkstra(d,v,adjList)
-    
-    let max = 0;
-    for(let i=1;i<d.length;i++) max = Math.max(max,d[i]);
-    
-    let answer = 0;
-    for(let i=1;i<d.length;i++) if(d[i]==max) answer++;
-    
-    return answer;
-}
-
-function Dijkstra(d,v,adjList){
-    let pque = [];
-    pque.push([1,d[1]]);
-    while(pque.length > 0){
-        let data = pque.shift();
-        if(v[data[0]]) continue;
+    while(queue.length > 0){
+        let data = queue.shift();
+        let vertex = data[0];
+        let distance = data[1];
+        if(v[vertex]) continue;
         
-        v[data[0]]=true;
-        
-        let vertex = adjList[data[0]];
-        for(let i=0;i<vertex.length;i++){
-            if(!v[vertex[i]] && d[vertex[i]] >1+d[data[0]]){
-                d[vertex[i]] = 1+d[data[0]];
-                pque.push([vertex[i],d[vertex[i]]]);
-                pque.sort((a,b)=>{
-                    return a[1] - b[1];
-                })
+        for(let i=0;i<adjList[vertex].length;i++){
+            if(!v[adjList[vertex][i]] 
+               && d[adjList[vertex][i]] > distance+1){
+                d[adjList[vertex][i]] = distance+1;
+                queue.push([adjList[vertex][i],d[adjList[vertex][i]]]);
+                queue.sort((a,b)=> a[1]-b[1]);
             }
         }
     }
+    
+    let max = 0;
+    let answer = 0;
+    for(let i=1;i<d.length;i++) max = Math.max(max,d[i]);
+    for(let i=1;i<d.length;i++) if(max==d[i]) answer++;
+    return answer;
 }
