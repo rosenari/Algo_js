f48a631d4d0248b77dd60e12ddc7ba75c14ec8dc
임장순 <rosenari88@gmail.com>
2021-05-03 19:18:57 +0900

    Fix / 프로그래머스 1차비밀지도

--- 2차원배열/프로그래머스_1차비밀지도.js
+++ 2차원배열/프로그래머스_1차비밀지도.js
@@ -1,28 +1,13 @@
 function solution(n, arr1, arr2) {
-    
+    let answer = [];
     for(let i=0;i<n;i++){
-        arr1[i] = arr1[i].toString(2).split("");
-        
-        while(arr1[i].length < n) arr1[i].unshift('0');
-        
-        arr2[i] = arr2[i].toString(2).split("");
-        
-        while(arr2[i].length < n) arr2[i].unshift('0');
-    }
-    
-    let matrix = [];
-    
-    for(let i=0;i<n;i++){
-        matrix.push(new Array(n).fill(""));
-        for(let j=0;j<n;j++){
-            if(arr1[i][j]=='1' || arr2[i][j]=='1') matrix[i][j] = '#';
-            else matrix[i][j] = ' ';
+        let binary = (arr1[i] | arr2[i]).toString(2);
+        while(binary.length<n) binary = "0"+binary;
+        for(let i=0;i<n;i++){
+            binary=binary.replace("0"," ",i);
+            binary=binary.replace("1","#",i);
         }
+        answer.push(binary);
     }
-    
-    for(let i=0;i<n;i++){
-        matrix[i] = matrix[i].join("");
-    }
-    
-    return matrix;
+    return answer;
 }
