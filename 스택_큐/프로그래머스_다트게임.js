a9263fda7faafdb429404193548b733f79d12609
임장순 <rosenari88@gmail.com>
2021-05-03 21:06:05 +0900

    Fix / 프로그래머스 [1차] 다트게임

--- 스택_큐/프로그래머스_다트게임.js
+++ 스택_큐/프로그래머스_다트게임.js
@@ -1,36 +1,28 @@
 function solution(dartResult) {
+    let scores = [];
     dartResult = dartResult.split("");
-    let numbers = [];
-    
-    let num = "";
     for(let i=0;i<dartResult.length;i++){
-        if(dartResult[i]=='S' ||dartResult[i]=='D' || dartResult[i]=='T'){
-            let sum = 0;            
-            if(dartResult[i]=='S') sum = parseInt(num);
-            else if(dartResult[i]=='D') sum = Math.pow(parseInt(num),2);
-            else if(dartResult[i]=='T') sum = Math.pow(parseInt(num),3);
-            numbers.push(sum);
-            num = "";
-        }else {
-            if(dartResult[i]!=='*' && dartResult[i]!=='#') num+=dartResult[i];
-            else{
-                if(dartResult[i]=='*'){
-                    if(numbers.length >= 2){
-                        numbers[numbers.length-1] =numbers[numbers.length-1]*2; 
-                        numbers[numbers.length-2] =numbers[numbers.length-2]*2; 
-                    }else{
-                        numbers[numbers.length-1] =numbers[numbers.length-1]*2; 
-                    }
-                }else {
-                    numbers[numbers.length-1] =numbers[numbers.length-1]*-1; 
-                }
-            }
-        } 
+        if(dartResult[i]=='S'){
+            if(dartResult[i-1]=="0" 
+               && dartResult[i-2]=="1") scores.push(Math.pow(10,1));
+            else scores.push(Math.pow(dartResult[i-1],1));
+        }else if(dartResult[i]=='D'){
+            if(dartResult[i-1]=="0" 
+               && dartResult[i-2]=="1") scores.push(Math.pow(10,2));
+            else scores.push(Math.pow(dartResult[i-1],2));
+        }else if(dartResult[i]=='T'){
+            if(dartResult[i-1]=="0" 
+               && dartResult[i-2]=="1") scores.push(Math.pow(10,3));
+            else scores.push(Math.pow(dartResult[i-1],3));
+        }else if(dartResult[i]=='*'){
+            if(scores.length > 0) scores[scores.length-1] *= 2;
+            if(scores.length > 1) scores[scores.length-2] *= 2;
+        }else if(dartResult[i]=='#'){
+            if(scores.length > 0) scores[scores.length-1] *= -1;
+        }
     }
     let answer = 0;
-    while(numbers.length > 0){
-        answer += numbers.pop();
-    }
+    while(scores.length > 0) answer+=scores.shift();
     
     return answer;
 }
