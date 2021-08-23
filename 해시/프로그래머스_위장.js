8454a4d4164061c9bb9e44afea4f8a2032369388
임장순 <49670068+rosenari@users.noreply.github.com>
2021-08-23 18:33:27 +0900

    Update 프로그래머스_위장.js

--- 해시/프로그래머스_위장.js
+++ 해시/프로그래머스_위장.js
@@ -1,14 +1,13 @@
 //프로그래머스 LEVEL2
 
 function solution(clothes) {
-    var answer = 1;
-    let map = [];
-    clothes.forEach((arr) => {
-        if (!map[arr[1]]) map[arr[1]] = 1;
-        else map[arr[1]]++;
-    })
-
-    for (let key in map) answer *= (map[key] + 1);
-
+    let answer = 1;
+    const map = new Map();
+    clothes.forEach(([name ,type]) => {
+        map.set(type, (map.get(type) || 0) + 1);
+    });
+    map.forEach(num => {
+        answer *= (num + 1);
+    });
     return answer - 1;
 }
