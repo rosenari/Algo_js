e79d5736c80a0f355c0df1e35faf844b7361c4ff
임장순 <49670068+rosenari@users.noreply.github.com>
2021-08-24 10:51:33 +0900

    Update 프로그래머스_기능개발.js

--- 스택_큐/프로그래머스_기능개발.js
+++ 스택_큐/프로그래머스_기능개발.js
@@ -1,12 +1,10 @@
 function solution(progresses, speeds) {
-    let arr = [];
-    for(let i=0;i<progresses.length;i++){
-        let remain = Math.ceil((100-progresses[i])/speeds[i]);
-        if(arr.length > 0 && arr[arr.length-1][0]>=remain) arr[arr.length-1][1]+=1;
-        else arr.push([remain,1]);
-    }
-    let answer = [];
-    for(let i=0;i<arr.length;i++) answer.push(arr[i][1]);
-    
+    const answer = [];
+    progresses.reduce((max, progress, index) => { 
+        const remain = Math.ceil((100 - progress) / speeds[index]);
+        if((max < remain) && answer.push(1)) return remain;
+        answer[answer.length - 1] += 1;
+        return max;
+    }, -1);
     return answer;
 }
