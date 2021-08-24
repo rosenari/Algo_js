9c59f44f0f44fe565d9ab72ce783a6a89edb12a2
임장순 <49670068+rosenari@users.noreply.github.com>
2021-08-24 10:52:39 +0900

    Update 프로그래머스_기능개발.js

--- 스택_큐/프로그래머스_기능개발.js
+++ 스택_큐/프로그래머스_기능개발.js
@@ -3,8 +3,7 @@ function solution(progresses, speeds) {
     progresses.reduce((max, progress, index) => { 
         const remain = Math.ceil((100 - progress) / speeds[index]);
         if((max < remain) && answer.push(1)) return remain;
-        answer[answer.length - 1] += 1;
-        return max;
+        answer[answer.length - 1] += 1; return max;
     }, -1);
     return answer;
 }
