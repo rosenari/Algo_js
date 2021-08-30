4ff4bef5f0c27de2d607e10a454aaffdd7d2449e
임장순 <49670068+rosenari@users.noreply.github.com>
2021-08-30 23:47:44 +0900

    Update 프로그래머스_프린터.js

--- 스택_큐/프로그래머스_프린터.js
+++ 스택_큐/프로그래머스_프린터.js
@@ -1,26 +1,16 @@
 //프로그래머스 LEVEL2
 
 function solution(priorities, location) {
-    let answer = 0;
-    let queue = priorities.map((v, i) => {
-        return [v, i];
-    });
-
-    while (queue.length > 0) {
-        let doc = queue.shift();
-        let p = false;
-        for (let i in queue) {
-            if (doc[0] < queue[i][0]) {
-                p = true;
-                break;
-            }
-        }
-        if (p) queue.push(doc);
-        else {
+    let answer = 1;
+    priorities = priorities.map((priority, index) => [priority, index]);
+    while(priorities.length > 0) {
+        const current = priorities.shift();
+        if(priorities.find(priority => current[0] < priority[0])){
+            priorities.push(current);
+        }else{
+            if(current[1] === location) break;
             answer++;
-            if (doc[1] == location) break;
         }
     }
-
     return answer;
 }
