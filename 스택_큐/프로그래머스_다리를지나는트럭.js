77fd375de2c0cc8177f2b782ca6de0e0d3640206
임장순 <49670068+rosenari@users.noreply.github.com>
2021-08-31 00:58:48 +0900

    Update 프로그래머스_다리를지나는트럭.js

--- 스택_큐/프로그래머스_다리를지나는트럭.js
+++ 스택_큐/프로그래머스_다리를지나는트럭.js
@@ -1,28 +1,16 @@
 function solution(bridge_length, weight, truck_weights) {
     let answer = 0;
     let bridge = [];
-    while(truck_weights.length > 0 || bridge.length > 0){
-        let sum = 0;
-        if(bridge.length > 0){
-            for(let i=0;i<bridge.length;i++){
-                let truck = bridge[i];
-                truck[1]+=1;
-                if(truck[1] >= bridge_length){
-                    bridge.shift();
-                    i--;
-                }else{
-                    sum+=truck[0];
-                }
+    while(truck_weights.length > 0 || bridge.length > 0) {
+        bridge = bridge.filter(truck => truck[1] < bridge_length);
+        bridge = bridge.map(truck => [truck[0], truck[1] + 1]);
+        const bridge_weight = bridge.reduce((sum, truck) => sum + truck[0], 0);
+        if(truck_weights.length > 0 && bridge_weight + truck_weights[0] <= weight) {
+            if(bridge.length === 0 || bridge.length < bridge_length){
+                bridge.push([truck_weights.shift(), 1]);
             }
         }
-        
-        if(truck_weights.length > 0){
-            if(sum+truck_weights[0] <= weight && bridge.length < bridge_length){
-                bridge.push([truck_weights.shift(),0]);
-            }
-        }
-        
         answer++;
-    }    
+    }
     return answer;
 }
