d4a12caeb11020bfba463ed70c522641f65d6af1
임장순 <49670068+rosenari@users.noreply.github.com>
2021-08-31 17:50:45 +0900

    Update 프로그래머스_디스크컨트롤러.js

--- 힙/프로그래머스_디스크컨트롤러.js
+++ 힙/프로그래머스_디스크컨트롤러.js
@@ -1,34 +1,21 @@
 function solution(jobs) {
-    let N = jobs.length;
-    //요청시간으로 오름차순 정렬
-    jobs.sort((a,b) => a[0]-b[0]);
-    
+    const N = jobs.length;
     let answer = 0;
-    let last = 0;
-    
-    let queue = [];
+    let prevLast = 0;
+    const queue = [];
+    jobs.sort((a, b) => a[0] - b[0]);
     while(jobs.length > 0 || queue.length > 0){
-        
-        while(jobs.length > 0){
-            let job2 = jobs[0];
-            if(last >= job2[0]){
-                queue.push(jobs.shift());
-            }else{
-                break;
-            }
-        }
-        
-        if(queue.length >0){
-            queue.sort((a,b) => {
-                return (last+a[1]-a[0])-(last+b[1]-b[0])
-            });
-            let job2 = queue.shift();
-            last += job2[1];
-            answer += (last-job2[0]);
-        }else {
-            last =jobs[0][0];
+        jobs.filter(job => prevLast >= job[0]).forEach(job => queue.push(job));
+        jobs = jobs.filter(job => prevLast < job[0]);
+        if(queue.length > 0){
+            queue.sort((a, b) => a[1] - b[1]);
+            const currentJob = queue.shift();
+            prevLast += currentJob[1];
+            answer += prevLast - currentJob[0];
+        }else{
+            prevLast = jobs[0][0];
         }
     }
-    
-    return parseInt(answer/N);
+    answer = parseInt(answer / N);
+    return answer;
 }
