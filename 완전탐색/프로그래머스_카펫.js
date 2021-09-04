2b15284381bb22257be8bd8c15e4549d37abf615
임장순 <49670068+rosenari@users.noreply.github.com>
2021-09-04 12:07:43 +0900

    Update 프로그래머스_카펫.js

--- 완전탐색/프로그래머스_카펫.js
+++ 완전탐색/프로그래머스_카펫.js
@@ -4,6 +4,6 @@ function solution(brown, yellow) {
         const sum = brown + yellow;
         if(sum % height !== 0) continue;
         const width = parseInt(sum / height);
-        if(width >= height && ((width - 2) * (height - 2)) === yellow) return [width, height]; //width - 2 * height - 2가 yellow 격자의 개수임도 확인해야함.
+        if(width >= height && ((width - 2) * (height - 2)) === yellow) return [width, height]; //(width - 2) * (height - 2)가 yellow 격자의 개수임도 확인해야함.
     }
 }
