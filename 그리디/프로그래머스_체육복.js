80bff4a0bc39bce4638e1d5d8173bac0e7a0c1ef
임장순 <49670068+rosenari@users.noreply.github.com>
2021-09-05 14:37:58 +0900

    Update 프로그래머스_체육복.js

--- 그리디/프로그래머스_체육복.js
+++ 그리디/프로그래머스_체육복.js
@@ -1,31 +1,13 @@
-//프로그래머스 LEVEL1
-
 function solution(n, lost, reserve) {
-    let answer = n;
-
-    lost = lost.filter((v) => {
-        const s = reserve.indexOf(v);
-        reserve = reserve.filter((r) => v - r !== 0);
-
-        return s === -1;
+    reserve.sort((a, b) => a - b);
+    reserve = reserve.filter(r => {
+        const rental = lost.find(l => l === r);
+        if(rental) lost = lost.filter(l => l !== rental);
+        return !rental;
     });
-
-    lost = lost.filter((v) => {
-        const f = reserve.indexOf(v - 1);
-        if (f !== -1) {
-            reserve.splice(f, 1);
-            return false;
-        }
-        const b = reserve.indexOf(v + 1);
-        if (b !== -1) {
-            reserve.splice(b, 1);
-            return false;
-        }
-
-        return true;
+    reserve.forEach(r => {
+            const rental = lost.find(l => l === r - 1) || lost.find(l => l === r + 1);
+            if(rental) lost = lost.filter(l => l !== rental);
     });
-
-    answer -= lost.length;
-
-    return answer;
+    return n - lost.length;
 }
