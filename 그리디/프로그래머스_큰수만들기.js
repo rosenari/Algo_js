7b68dbb3c6e9cb13cbb44b2e25a50ea278308568
임장순 <49670068+rosenari@users.noreply.github.com>
2021-09-06 14:47:22 +0900

    Update 프로그래머스_큰수만들기.js

--- 그리디/프로그래머스_큰수만들기.js
+++ 그리디/프로그래머스_큰수만들기.js
@@ -1,14 +1,13 @@
 function solution(number, k) {
-    number = number.split("");
-    let stack = [];
-    for(let i=0;i<number.length;i++){
-        while(k > 0 && stack[stack.length -1] < number[i]){
-            stack.pop();
-            k--;
+    const answer = [];
+    number = Array.from(number);
+    number.forEach((pick, index) => {
+        while(answer[answer.length - 1] < pick && k > 0){
+                answer.pop();
+                k--;
         }
-        stack.push(number[i]);
-    }
-    if(k > 0) stack = stack.slice(0,stack.length - k);
-    
-    return stack.join("");
+        answer.push(pick);
+    });
+    if(k > 0) answer.pop();
+    return answer.join('');
 }
