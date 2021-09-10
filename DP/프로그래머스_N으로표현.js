d2802b726d184bd55d15f3f3aa9817d2c5d8ca2e
임장순 <49670068+rosenari@users.noreply.github.com>
2021-09-10 14:27:45 +0900

    Update 프로그래머스_N으로표현.js

--- DP/프로그래머스_N으로표현.js
+++ DP/프로그래머스_N으로표현.js
@@ -1,30 +1,20 @@
 function solution(N, number) {
-    if(number==N) return 1;
-    
-    let dp = [new Set([])];
-    
-    let num = N;
-    for(let i=1;i<=8;i++){
-        dp.push(new Set([num]));
-        num += N*Math.pow(10,i);
-    }
-    
-    for(let n=2;n<=8;n++){
-        
-        for(let s=1;s<n;s++){
-            for(let v of dp[s]){
-                for(let v2 of dp[n-s]){
-                    dp[n].add(v+v2);
-                    dp[n].add(v-v2);
-                    dp[n].add(v*v2);
-                    if(v2!==0) dp[n].add(v/v2);
+    const MAX = 8;
+    if(N === number) return 1;
+    const dp = Array.from({length: MAX + 1}).map((_, i) => new Set([parseInt(`${N}`.repeat(i))]));
+    for(let n = 2; n < dp.length; n++) {
+        for(let s = 1; s < n; s++){
+            for(const v1 of dp[s]){
+                for(const v2 of dp[n-s]){
+                    dp[n].add(v1 + v2);
+                    dp[n].add(v1 - v2);
+                    dp[n].add(v1 * v2);
+                    if(v2 !== 0) dp[n].add(parseInt(v1 / v2));
                 }
             }
         }
-        
-        
         if(dp[n].has(number)) return n;
     }
-    
+    if(parseInt(`${N}`.repeat(MAX)) === number) return MAX;
     return -1;
 }
