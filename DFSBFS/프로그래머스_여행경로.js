84d5d69c8afc0354594296f16bce3e16a22a9383
임장순 <49670068+rosenari@users.noreply.github.com>
2021-09-13 10:57:16 +0900

    Update 프로그래머스_여행경로.js

--- DFSBFS/프로그래머스_여행경로.js
+++ DFSBFS/프로그래머스_여행경로.js
@@ -1,30 +1,25 @@
+let answer;
 function solution(tickets) {
-    var answer = [];
-    tickets.sort((a, b) => {
-        return a[1].localeCompare(b[1]);
-    });
-    console.log(tickets);
-    v = new Array(tickets.length).fill(false);
-    answer.push("ICN");
-    DFS("ICN", tickets, answer);
-
+    const N = tickets.length + 1;
+    const v = Array.from({length:N}).fill(false);
+    const routes = ["ICN"];
+    tickets.sort((a,b) => a[1].localeCompare(b[1]));
+    DFS("ICN", tickets, N, v, routes);
     return answer;
 }
-let v;
-let is = false;
-let DFS = (pspt, tickets, answer) => {
-    if (answer.length == tickets.length + 1) {
-        is = true;
-        return;
-    }
 
-    for (let i in tickets) {
-        if (!v[i] && tickets[i][0] == pspt) {
+function DFS(passport, tickets, N, v, routes){
+    if(routes.length === N){
+        answer = routes;
+        return;
+    }    
+    
+    for(const i in tickets){
+        if(answer) return;
+        const [s, d] = tickets[i];
+        if(!v[i] && passport === s){
             v[i] = true;
-            answer.push(tickets[i][1]);
-            DFS(tickets[i][1], tickets, answer);
-            if (is) return;
-            answer.pop();
+            DFS(d, tickets, N, v, [...routes, d]);
             v[i] = false;
         }
     }
