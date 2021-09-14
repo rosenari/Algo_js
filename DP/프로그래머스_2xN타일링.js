53ef5d0134781d3c3c79cfcf9a346e47896d5b49
임장순 <49670068+rosenari@users.noreply.github.com>
2021-09-14 14:09:44 +0900

    Update 프로그래머스_2xN타일링.js

--- DP/프로그래머스_2xN타일링.js
+++ DP/프로그래머스_2xN타일링.js
@@ -1,15 +1,11 @@
 function solution(n) {
-    var answer = 0;
-    let arr = new Array(n+1);
-    if(n==1) return 1;
-    if(n==2) return 2;
-    arr[1] = 1;
-    arr[2] = 2;
-    
-    for(let i=3;i<=n;i++){
-        arr[i] = arr[i-1]+arr[i-2];
-        arr[i]%=1000000007
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+    const arr = Array.from({length:n});
+    arr[0] = 1;
+    arr[1] = 2;
+    for(let i=2;i<n;i++){
+        arr[i] = (arr[i-1] + arr[i-2]) % 1000000007
     }
-        
-    return arr[n];
+    return arr[n-1];
 }
