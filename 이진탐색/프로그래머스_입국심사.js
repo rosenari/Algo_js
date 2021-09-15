b789195837b7ffdf59d80719bef87eb430fa2e3e
임장순 <49670068+rosenari@users.noreply.github.com>
2021-09-15 12:24:55 +0900

    Update 프로그래머스_입국심사.js

--- 이진탐색/프로그래머스_입국심사.js
+++ 이진탐색/프로그래머스_입국심사.js
@@ -1,30 +1,17 @@
 function solution(n, times) {
     times.sort((a,b) => a-b);
-    let max = times[times.length-1] * n;
-    let min = 0;
-    
-    return binarySearch(max,min,times,n); 
-}
-
-function binarySearch(max,min,times,n){
-    let answer = max;
-    
-    while(min <= max){
-        
-        let sum = 0;
-        let mid = parseInt((max+min)/2);
-        
-        for(let time of times){
-            sum+=parseInt(mid/time);
-        }
-        
-        if(sum >= n){
-            max = mid-1;
-            answer = Math.min(answer,mid);
-        }else{
-            min = mid+1;
+    let left = 0;
+    let right = n * times[times.length - 1];
+    let answer = right;
+    while(left <= right){
+        const mid = Math.floor((left + right) / 2);
+        const sum = times.reduce((a,v) => a + Math.floor(mid/v),0); //sum은 걸리는 시간이 mid일때, 각 심시관이 처리할수 있는 사람의 수
+        if(n > sum) { //심사관이 처리할 수 있는 사람의 수가 처리해야할 사람수보다 적으면 안됨.
+            left = mid + 1;
+        } else { //심사관이 처리할 수 있는 사람의 수가 처리해야할 사람의 수보다 많거나 같으면 상관없음.
+            answer = Math.min(mid, answer);
+            right = mid - 1;
         }
     }
-    
     return answer;
 }
