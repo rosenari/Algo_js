59b22ad7470b916c21349dfd9d55ce5f7a8bcd36
임장순 <49670068+rosenari@users.noreply.github.com>
2021-10-06 17:08:43 +0900

    Update 프로그래머스_기능개발.js

--- 스택_큐/프로그래머스_기능개발.js
+++ 스택_큐/프로그래머스_기능개발.js
@@ -2,11 +2,8 @@ function solution(progresses, speeds) {
     const answer = [];
     progresses.forEach((p,i) => {
         const deplay = Math.ceil((100 - p) / speeds[i]);
-        if(answer.length == 0) answer.push({ deplay ,cnt: 1 });
-        else{
-            if(answer[answer.length - 1].deplay >= deplay) answer[answer.length - 1].cnt += 1;
-            else answer.push({ deplay ,cnt: 1 });
-        }
+        if(answer.length > 0 && answer[answer.length - 1].deplay >= deplay) answer[answer.length - 1].cnt += 1;
+        else answer.push({ deplay ,cnt: 1 });
     });
     return answer.map(v => v.cnt);
 }
