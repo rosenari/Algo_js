4e436470fe347d7b44c7e43375d9b64adfbb6769
임장순 <49670068+rosenari@users.noreply.github.com>
2021-10-07 00:51:34 +0900

    Update 프로그래머스_가장큰수.js

--- 정렬/프로그래머스_가장큰수.js
+++ 정렬/프로그래머스_가장큰수.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
-    const answer = numbers.sort((a, b) => ('' + b + a) - ('' + a + b)).join('');
+    const answer = numbers.sort((a,b) => [b,a].join('') - [a,b].join('')).join('');
     return parseInt(answer) === 0 ? '0' : answer;
 }
