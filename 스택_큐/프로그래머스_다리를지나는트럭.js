c070333f7b1d42b4d2e6ab66f47033670b63ef01
임장순 <49670068+rosenari@users.noreply.github.com>
2021-10-06 19:46:22 +0900

    Update 프로그래머스_다리를지나는트럭.js

--- 스택_큐/프로그래머스_다리를지나는트럭.js
+++ 스택_큐/프로그래머스_다리를지나는트럭.js
@@ -1,15 +1,12 @@
 function solution(bridge_length, weight, truck_weights) {
     let answer = 0;
     let bridge = [];
-    while(truck_weights.length > 0 || bridge.length > 0) {
-        bridge = bridge.filter(truck => truck[1] < bridge_length);
-        bridge = bridge.map(truck => [truck[0], truck[1] + 1]);
-        const bridge_weight = bridge.reduce((sum, truck) => sum + truck[0], 0);
-        if(truck_weights.length > 0 && bridge_weight + truck_weights[0] <= weight) {
-            if(bridge.length === 0 || bridge.length < bridge_length){
-                bridge.push([truck_weights.shift(), 1]);
-            }
+    while(truck_weights.length > 0 || bridge.length > 0){
+        bridge = bridge.filter(truck => truck.time < bridge_length);
+        if(truck_weights.length > 0 && (bridge.reduce((sum, truck) => sum + truck.weight, 0) + truck_weights[0]) <= weight && bridge.length < bridge_length){
+            bridge.push({ weight: truck_weights.shift(), time: 0 });
         }
+        bridge = bridge.map(truck => ({ ...truck, time: truck.time + 1 }));
         answer++;
     }
     return answer;
