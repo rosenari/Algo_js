ff7731bb48d4a28556c9f93b34f4e5948f6d966a
임장순 <49670068+rosenari@users.noreply.github.com>
2021-10-08 22:05:01 +0900

    Update 프로그래머스_보석쇼핑.js

--- 투포인터/프로그래머스_보석쇼핑.js
+++ 투포인터/프로그래머스_보석쇼핑.js
@@ -1,38 +1,21 @@
 function solution(gems) {
-    let answers = [];
-    let start = 0;
-    let end = 0;
-    let N = gems.length;
-    let target = new Set();
-    for(let i=0;i<gems.length;i++){
-        if(!target.has(gems[i])) target.add(gems[i]);
-    }
-    let now = new Map();
-    now.set(gems[0],1);
-    while(end < N){
-        if(now.size==target.size){
-            answers.push([start,end]);
-            if(now.has(gems[start])){
-                let data = now.get(gems[start]);
-                if(data > 1) now.set(gems[start],data-1);
-                else now.delete(gems[start]);
-            }
-            start++;
-        }else{
-            end++;
-            if(!now.has(gems[end])) now.set(gems[end],1);
-            else now.set(gems[end],now.get(gems[end])+1);
+    let s = 0, e = 0;
+    const answer = [];
+    const set = new Set(gems);
+    const now = new Map();
+    now.set(gems[s], 1);
+    while(e < gems.length){
+        if(now.size === set.size){
+            const num = now.get(gems[s]);
+            if(num > 1) now.set(gems[s], num - 1);
+            else now.delete(gems[s]);
+            answer.push([s + 1, e + 1]);
+            s++;
+        }else {
+            e++;
+            const num = now.get(gems[e]) || 0;
+            now.set(gems[e], num + 1);
         }
     }
-    
-    answers.sort((a,b)=> {
-        if((a[1]-a[0]) === (b[1]-b[0])){
-            return a[0]-b[0];
-        }else{
-            return (a[1]-a[0]) - (b[1]-b[0]);
-        }
-    })
-    let answer = answers.shift();
-    
-    return [(+answer[0])+1,(+answer[1])+1]
+    return answer.sort((a, b) => ((a[1] - a[0]) - (b[1] - b[0])))[0];
 }
