dc96306a6c05134fbb3ad0a98db6222555083b0b
임장순 <49670068+rosenari@users.noreply.github.com>
2021-10-08 18:51:37 +0900

    Update 프로그래머스_가장먼노드.js

--- 다익스트라/프로그래머스_가장먼노드.js
+++ 다익스트라/프로그래머스_가장먼노드.js
@@ -1,42 +1,41 @@
 function solution(n, edge) {
-    return Djikstra(n,edge);
+    const list = Array.from({ length: n + 1 }).map(_ => []);
+    const v = new Array(n + 1).fill(false);
+    edge.forEach(([s, d]) => {
+        list[s].push(d);
+        list[d].push(s);
+    });
+    
+    const d = Dijkstra(list, n);
+    const max = Math.max(...d);
+    return d.filter(v => v === max).length;
 }
 
-function Djikstra(n,edge){
-    let adjList = new Array(n+1);
-    for(let i=0;i<adjList.length;i++) adjList[i] = [];
-    for(let i=0;i<edge.length;i++){
-        let from = edge[i][0];
-        let to = edge[i][1];
-        adjList[from].push(to);
-        adjList[to].push(from);
-    }
-    
-    let v = new Array(n+1).fill(false);
-    let d = new Array(n+1).fill(Infinity);
-    d[1] = 0;
-    let queue = [];
-    queue.push([1,d[1]]);
-    
+function Dijkstra(list, n) {
+    const queue = [];
+    let cnt = 0;
+    const v = new Array(n + 1).fill(false);
+    const d = new Array(n + 1).fill(Infinity);
+    d[0] = 0, d[1] = 0;
+    queue.push({ index: 1, distance: d[1] });
+
     while(queue.length > 0){
-        let data = queue.shift();
-        let vertex = data[0];
-        let distance = data[1];
-        if(v[vertex]) continue;
+        const { index, distance } = queue.shift();
+        if(v[index]) continue;
+        v[index] = true;
         
-        for(let i=0;i<adjList[vertex].length;i++){
-            if(!v[adjList[vertex][i]] 
-               && d[adjList[vertex][i]] > distance+1){
-                d[adjList[vertex][i]] = distance+1;
-                queue.push([adjList[vertex][i],d[adjList[vertex][i]]]);
-                queue.sort((a,b)=> a[1]-b[1]);
+        if(cnt++ == n) return d;        
+        
+        for(const node of list[index]){
+            if(!v[node] && distance + 1 < d[node] ){
+                d[node] = distance + 1;
+                queue.push({ index: node, distance: d[node] });
+                queue.sort((a,b) => {
+                    return a.distance - b.distance;
+                });
             }
         }
     }
     
-    let max = 0;
-    let answer = 0;
-    for(let i=1;i<d.length;i++) max = Math.max(max,d[i]);
-    for(let i=1;i<d.length;i++) if(max==d[i]) answer++;
-    return answer;
+    return d;
 }
