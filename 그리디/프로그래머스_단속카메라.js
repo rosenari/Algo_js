5789214ffae8a72f9519220ce11183664df0772a
임장순 <49670068+rosenari@users.noreply.github.com>
2022-07-16 12:57:33 +0900

    Update 프로그래머스_단속카메라.js

--- 그리디/프로그래머스_단속카메라.js
+++ 그리디/프로그래머스_단속카메라.js
@@ -1,12 +1,11 @@
 function solution(routes) {
-    let camera = -30001;
-    let answer = 0;
-    routes.sort((a, b) => a[1] - b[1]);
-    routes.forEach(route => {
-        if(camera < route[0]){
-            camera = route[1];
-            answer ++;
+    let max = -30001;
+    return routes.sort((pre, next) => pre[1] - next[1])
+        .filter((route) => {
+        if(route[0] > max) {
+            max = route[1];
+            return true;
         }
-    });
-    return answer;
+        return false;
+    }).length;
 }
